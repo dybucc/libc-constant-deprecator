a7a6e19480a8786d6b4d4074f4672bd024657284
dybucc <149513579+dybucc@users.noreply.github.com>
2026-04-30 19:02:49 +0200

    chore: keep working on the binary

--- libc-constant-deprecator-bin/src/main.rs
+++ libc-constant-deprecator-bin/src/main.rs
@@ -1,11 +1,14 @@
 use std::{env, path::PathBuf};
 
-use anyhow::bail;
 use clap::Parser;
-use futures::future;
+use crossterm::{
+    event::{Event, EventStream, KeyCode, KeyEvent, KeyModifiers},
+    terminal,
+};
+use futures::{StreamExt, future};
 use libc_constant_deprecator_lib::ConstContainer;
 use tokio::{
-    sync::oneshot::{self, Receiver, Sender},
+    sync::mpsc::{self, UnboundedReceiver, UnboundedSender, error::TryRecvError},
     task,
 };
 
@@ -14,12 +17,125 @@ pub(crate) struct Args {
     path: Option<String>,
 }
 
+#[derive(Debug)]
+#[non_exhaustive]
+pub(crate) struct State {
+    events: UnboundedReceiver<RawUserEvent>,
+}
+
+impl State {
+    pub(crate) fn new(events_channel: UnboundedReceiver<RawUserEvent>) -> Self {
+        Self {
+            events: events_channel,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct UserEvent {
+    repr: UserEventRepr,
+}
+
+impl UserEvent {
+    pub(crate) fn new(event: UserEventRepr) -> Self {
+        Self { repr: event }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct RawUserEvent {
+    repr: RawUserEventRepr,
+}
+
+impl RawUserEvent {
+    pub(crate) fn new(event: RawUserEventRepr) -> Self {
+        Self { repr: event }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum RawUserEventRepr {
+    PlainText(char),
+    Return,
+    ShiftReturn,
+    Space,
+    Escape,
+}
+
+#[derive(Debug)]
+pub(crate) enum UserEventRepr {
+    // Corresponds with plain text user input at the prompt.
+    TextualInput(String),
+    // Is triggered with the return key and should trigger a filtering event with the last saved
+    // regex input at the prompt.
+    Search,
+    // Is triggered with the space key and should toggle all selected constants' state to
+    // "deprecated", unless all selected constants are already deprecated, in which case it should
+    // undeprecate.
+    Toggle,
+    // Is triggered with the shift + return combo and should effect the changes to disk.
+    Effect,
+    // Is triggered with the escape key and should clear the currently input regex.
+    Clear,
+}
+
 #[expect(clippy::unused_async, reason = "WIP.")]
-pub(crate) async fn render(constants: &mut ConstContainer, rx: Receiver<()>) -> anyhow::Result<()> {
+pub(crate) async fn render(constants: ConstContainer, state: State) -> anyhow::Result<()> {
+    // Every time the rendering loop iterates, it ought send the current state of
+    // affairs within `state`, which contains information on the current input mode
+    // (either insert or normal mode.)
+    loop {
+        let State { events, .. } = &mut state;
+
+        match events.try_recv().map(|RawUserEvent { repr }| repr) {
+            Ok(RawUserEventRepr::PlainText(c)) => todo!(),
+            Ok(RawUserEventRepr::Return) => todo!(),
+            Ok(RawUserEventRepr::ShiftReturn) => todo!(),
+            Ok(RawUserEventRepr::Space) => todo!(),
+            Ok(RawUserEventRepr::Escape) => todo!(),
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break,
+        }
+    }
+
     Ok(())
 }
 
-pub(crate) async fn terminator(tx: Sender<()>) -> anyhow::Result<()> {
+#[expect(clippy::unused_async, reason = "WIP.")]
+pub(crate) async fn handle_input(channel: UnboundedSender<RawUserEvent>) -> anyhow::Result<()> {
+    let mut event_stream = EventStream::new().fuse();
+
+    while let Some(event) = event_stream.next().await {
+        match event? {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(' '),
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => _ = channel.send(RawUserEvent::new(RawUserEventRepr::Space)),
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(c),
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => _ = channel.send(RawUserEvent::new(RawUserEventRepr::PlainText(c))),
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => _ = channel.send(RawUserEvent::new(RawUserEventRepr::Return)),
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                modifiers: KeyModifiers::SHIFT,
+                ..
+            }) => _ = channel.send(RawUserEvent::new(RawUserEventRepr::ShiftReturn)),
+            Event::Key(KeyEvent {
+                code: KeyCode::Esc,
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => _ = channel.send(RawUserEvent::new(RawUserEventRepr::Escape)),
+            _ => (),
+        }
+    }
+
     Ok(())
 }
 
@@ -31,22 +147,19 @@ async fn main() -> anyhow::Result<()> {
         libc_constant_deprecator_lib::scan_files(env::current_dir()?).await?
     };
 
+    task::spawn_blocking(terminal::enable_raw_mode).await??;
+
     let mut parsed_constants = libc_constant_deprecator_lib::parse_constants(&files);
 
-    loop {
-        let (terminate_tx, terminate_rx) = oneshot::channel();
+    let (events_tx, events_rx) = mpsc::unbounded_channel();
 
-        match future::try_join(
-            task::spawn(render(&mut parsed_constants, terminate_rx)),
-            task::spawn(terminator(terminate_tx)),
-        )
-        .await
-        {
-            Ok((Ok(_), Ok(_))) => (),
-            Ok((Err(err), _) | (_, Err(err))) => bail!(err),
-            Err(task_err) => bail!(task_err),
-        }
+    let mut state = State::new(events_rx);
+
+    let input_handler = task::spawn(handle_input(events_tx));
+    let renderer = task::spawn(render(parsed_constants, state));
+
+    match future::try_join(input_handler, renderer).await? {
+        (Err(err), _) | (_, Err(err)) => Err(err),
+        _ => Ok(()),
     }
-
-    Ok(())
 }
